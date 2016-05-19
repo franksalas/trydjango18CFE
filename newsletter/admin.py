ac61f0806755695501c742c138dbc63bc724fae0
Frank Salas <Frank.salas@gmail.com>
2016-05-19 14:02:16 -0500

    Model Form. Created a form.py file to verify input from user.  Replaced Admin input.

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import SignUp
+from .forms import  SignUpForm
+
 
 
 class SignUpAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'timestamp', 'update']
+    form = SignUpForm
 
-    class Meta:
-        model = SignUp
+    # class Meta:
+    #     model = SignUp
 
 
 admin.site.register(SignUp, SignUpAdmin)
