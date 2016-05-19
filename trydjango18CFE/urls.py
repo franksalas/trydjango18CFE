fa6ec97b4e7f9abba5a04952814f14e91286b952
Frank Salas <Frank.salas@gmail.com>
2016-05-19 16:34:26 -0500

    Serving Static Files

--- trydjango18CFE/urls.py
+++ trydjango18CFE/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^contact/$', 'newsletter.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+# do not use on production
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.MEDIA_ROOT)
