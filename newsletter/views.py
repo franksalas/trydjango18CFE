3f6b8d96492d7dd9d1e2fb63b308563490c2bbc6
Frank Salas <Frank.salas@gmail.com>
2016-05-19 17:01:00 -0500

    Adding bootstrap to django

--- newsletter/views.py
+++ newsletter/views.py
@@ -15,7 +15,7 @@ def home(request):
         'title': title,
         'form': form,
     }
-    return render(request, 'home.html', context)
+    return render(request, 'base.html', context)
 
 
 def contact(request):
